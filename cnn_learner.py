020640d1c41ea37c1fd6aad9d9387c0ce2680f50
swow2015 <swow2015@POM-ITB-DGX01.campus.pomona.edu>
2021-04-20 18:41:21 -0700

    added recall and precision error metrics

--- cnn_learner.py
+++ cnn_learner.py
@@ -3,13 +3,16 @@ fastbook.setup_book()
 from fastbook import *
 from fastai.vision.widgets import *
 
-torch.cuda.set_device(2)
+torch.cuda.set_device(3)
 
-image_path = Path('./data/imgset1/')
+image_path = Path('/raid/heartnet/data/test_cnn_learner')
 images = get_image_files(image_path)
 
 invalid_images = verify_images(images)
 print(invalid_images)
+
+recall_function = Recall(pos_label=0)
+precision_function = Precision(pos_label=0)
 
 images_datablock = DataBlock(
     blocks=(ImageBlock, CategoryBlock), 
@@ -18,8 +21,8 @@ images_datablock = DataBlock(
     get_y=parent_label,
     batch_tfms=aug_transforms())
 
-dls = images_datablock.dataloaders(image_path, bs=16)
+dls = images_datablock.dataloaders(image_path, bs=4)
 
-learn = cnn_learner(dls, resnet18, metrics=error_rate)
+learn = cnn_learner(dls, resnet18, metrics=[error_rate, recall_function, precision_function])
 
-learn.fine_tune(4)
+learn.fine_tune(3)
