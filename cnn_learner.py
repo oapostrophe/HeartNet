65ae2aedfd3aaf9ffaf7ba18a08451bd08acd9c7
Sean O'Connor <swow2015@mymail.pomona.edu>
2021-05-30 17:21:50 -0700

    Update cnn_learner.py

--- cnn_learner.py
+++ cnn_learner.py
@@ -3,15 +3,19 @@ fastbook.setup_book()
 from fastbook import *
 from fastai.vision.widgets import *
 
+# Pick a GPU with free resources
 torch.cuda.set_device(2)
 
+# Get images
 image_path = Path('/raid/heartnet/data/imgset2')
 images = get_image_files(image_path)
 
+# Initialize metric functions
 recall_function = Recall(pos_label=0)
-# precision_function = Precision(pos_label=0)
-# f1_score = F1Score(pos_label=0)
+precision_function = Precision(pos_label=0)
+f1_score = F1Score(pos_label=0)
 
+# Initialize DataLoader
 images_datablock = DataBlock(
     blocks=(ImageBlock, CategoryBlock), 
     get_items=get_image_files, 
@@ -19,13 +23,9 @@ images_datablock = DataBlock(
     get_y=parent_label,
     batch_tfms=aug_transforms(do_flip=False)
 )
-# do_flip=False in aug_transforms?
-#item_tfms=Resize(400) under batch_tfms
-
 dls = images_datablock.dataloaders(image_path, bs=16)
 
-learn = cnn_learner(dls, resnet50, metrics=[error_rate, recall_function])
-
+# Create, train, and save model
+learn = cnn_learner(dls, resnet152, metrics=[error_rate, recall_function, precision_function, f1_score])
 learn.fine_tune(16)
-
 learn.export('demo_model_50.pkl')
